98cdebacaa5e269cdd14c324a331545552fa49ff
Sean Padden <sean.padden7@gmail.com>
2019-05-09 19:46:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,9 @@
 def my_collect(collection)
     i = 0
     while i < collection.length
-        yield if block_given?
+        if block_given? 
+          yield collection[i]
+        end 
     i += 1
     end
     collection
