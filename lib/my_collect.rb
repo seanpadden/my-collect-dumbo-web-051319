627a9f485fbeacb16fdc8fc4c6c430c694dd734f
Sean Padden <sean.padden7@gmail.com>
2019-05-09 19:59:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(collection)
     i = 0
     new_collection = []
-    while i < collection.length
-        end 
-    i += 1
-    end
-    collection
+      while i < collection.length
+        new_collection << yield collection[i]
+        i += 1
+      end
+    new_collection
 end
 
 
